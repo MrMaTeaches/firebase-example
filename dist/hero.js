c1732ff6a1cca8bbb675e1e4ac9b297761bc3859
Mike <mike.ma0426@gmail.com>
2024-06-06 08:09:10 -0400

    fixed hero

--- dist/hero.js
+++ dist/hero.js
@@ -3,26 +3,35 @@ import { nanoid } from "nanoid";
 class Hero {
     h = 25;
     w = 25;
-    x = Math.random() * Canvas.WIDTH - this.w;
-    y = Math.random() * Canvas.HEIGHT - this.h;
-    colour = `rgb(${this.randomizer(255)}, ${this.randomizer(255)}, ${this.randomizer(255)})`;
+    _x = Math.random() * Canvas.WIDTH - this.w;
+    _y = Math.random() * Canvas.HEIGHT - this.h;
+    _colour = `rgb(${this.randomizer(255)}, ${this.randomizer(255)}, ${this.randomizer(255)})`;
     moveSpeed = 1;
     id = nanoid(10);
+    get x() {
+        return this._x;
+    }
+    get y() {
+        return this._y;
+    }
+    get colour() {
+        return this._colour;
+    }
     moveRight() {
-        this.x += this.moveSpeed;
+        this._x += this.moveSpeed;
     }
     moveLeft() {
-        this.x -= this.moveSpeed;
+        this._x -= this.moveSpeed;
     }
     moveUp() {
-        this.y -= this.moveSpeed;
+        this._y -= this.moveSpeed;
     }
     moveDown() {
-        this.y += this.moveSpeed;
+        this._y += this.moveSpeed;
     }
     draw() {
-        Canvas.instance.context.fillStyle = this.colour;
-        Canvas.instance.context.fillRect(this.x, this.y, this.w, this.h);
+        Canvas.instance.context.fillStyle = this._colour;
+        Canvas.instance.context.fillRect(this._x, this._y, this.w, this.h);
     }
     randomizer(val) {
         return Math.floor(Math.random() * val);
--- src/hero.ts
+++ src/hero.ts
@@ -4,30 +4,42 @@ import { nanoid } from "nanoid";
 class Hero {
   private h: number = 25;
   private w: number = 25;
-  private x: number = Math.random() * Canvas.WIDTH - this.w;
-  private y: number = Math.random() * Canvas.HEIGHT - this.h;
-  private colour: string = `rgb(${this.randomizer(255)}, ${this.randomizer(
+  private _x: number = Math.random() * Canvas.WIDTH - this.w;
+  private _y: number = Math.random() * Canvas.HEIGHT - this.h;
+  private _colour: string = `rgb(${this.randomizer(255)}, ${this.randomizer(
     255
   )}, ${this.randomizer(255)})`;
   private moveSpeed = 1;
   private id: string = nanoid(10);
 
+  public get x(): number {
+    return this._x;
+  }
+
+  public get y(): number {
+    return this._y;
+  }
+
+  public get colour(): string {
+    return this._colour;
+  }
+
   public moveRight(): void {
-    this.x += this.moveSpeed;
+    this._x += this.moveSpeed;
   }
   public moveLeft(): void {
-    this.x -= this.moveSpeed;
+    this._x -= this.moveSpeed;
   }
   public moveUp(): void {
-    this.y -= this.moveSpeed;
+    this._y -= this.moveSpeed;
   }
   public moveDown(): void {
-    this.y += this.moveSpeed;
+    this._y += this.moveSpeed;
   }
 
   public draw(): void {
-    Canvas.instance.context.fillStyle = this.colour;
-    Canvas.instance.context.fillRect(this.x, this.y, this.w, this.h);
+    Canvas.instance.context.fillStyle = this._colour;
+    Canvas.instance.context.fillRect(this._x, this._y, this.w, this.h);
   }
 
   private randomizer(val: number): number {
